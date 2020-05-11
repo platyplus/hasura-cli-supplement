158c77698662f3440238334fd22d8dbd9ed1dfb2
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-05-11 21:44:00 +0200

    feat: install hasura cli if not installed

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log('bingo')
+const execCli = async (command: string, args?: string[], mute = true) => {
+  // TODO mute
+  const cli = Deno.run({
+    cmd: ['hasura', command, ...(args || [])],
+  })
+  await cli.status()
+  cli.close()
+}
+
+const installHasuraCli = async () => {
+  const bash = Deno.run({ cmd: ['bash'], stdin: 'piped' })
+  const installScript = await fetch(
+    'https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh'
+  )
+  await bash.stdin?.write(new Uint8Array(await installScript.arrayBuffer()))
+  bash.stdin?.close()
+  await bash.status()
+  bash.close()
+}
+
+try {
+  await execCli('version', [])
+} catch {
+  await installHasuraCli()
+}
+
+console.log('Done')
